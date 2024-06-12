18266c640abe776de56a91036693b2ba778979e9
Productivity <grantchen2021@gmail.com>
2024-06-12 19:07:22 -0400

    removed use of user_id and file_paths variables in ExtractStructure

--- file_segment_service/services/file_segment.py
+++ file_segment_service/services/file_segment.py
@@ -4,7 +4,6 @@ from file_segment_service.protobufs import (
     file_segment_service_pb2,
     file_segment_service_pb2_grpc,
 )
-
 from file_segment_service.database import FileChunksDatabase
 from file_segment_service import utils
 
@@ -13,15 +12,8 @@ class FileSegment(file_segment_service_pb2_grpc.FileSegmentServiceServicer):
     def ExtractStructure(self, request, context):
         print("ExtractStructure request received")
 
-        user_id = request.user_id
-        file_paths = request.file_paths
-
-        print(user_id, file_paths)
-
-        for file_path in file_paths:
-            file_chunks = FileChunksDatabase.get_file_chunks(
-                request.user_id, request.file_path
-            )
+        for file_path in request.file_paths:
+            file_chunks = FileChunksDatabase.get_file_chunks(request.user_id, file_path)
 
             sorted_file_chunks = sorted(
                 file_chunks, key=lambda file_chunk: file_chunk["contentchunkindex"]
