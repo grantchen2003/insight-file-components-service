b069b9a0d6f7fba09b6cf9c3417bb8702467fede
Productivity <grantchen2021@gmail.com>
2024-06-12 23:00:42 -0400

    made extract_file_segments concurrent

--- file_segment_service/services/file_segment.py
+++ file_segment_service/services/file_segment.py
@@ -1,4 +1,4 @@
-import base64
+import base64, concurrent.futures
 
 from file_segment_service.protobufs import (
     file_segment_service_pb2,
@@ -11,10 +11,8 @@ from file_segment_service import utils
 class FileSegment(file_segment_service_pb2_grpc.FileSegmentServiceServicer):
     def ExtractStructure(self, request, context):
         print("ExtractStructure request received")
-        
-        # make this concurrent/parallel
 
-        for file_path in request.file_paths:
+        def extract_file_segments(file_path):
             sorted_file_chunks = FileChunksDatabase.get_sorted_file_chunks(
                 request.user_id, file_path, "contentchunkindex"
             )
@@ -26,9 +24,16 @@ class FileSegment(file_segment_service_pb2_grpc.FileSegmentServiceServicer):
 
             source_code = "".join(decoded_contents)
 
-            for file_segment in utils.extract_file_structure(source_code):
-                yield file_segment_service_pb2.FileSegment(
+            return [
+                file_segment_service_pb2.FileSegment(
                     file_path=file_path,
                     start_line=file_segment["start_line"],
                     end_line=file_segment["end_line"],
                 )
+                for file_segment in utils.extract_file_structure(source_code)
+            ]
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for file_segments in executor.map(extract_file_segments, request.file_paths):
+                for file_segment in file_segments:
+                    yield file_segment
